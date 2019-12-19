9f5101879935954fd699b29786065db69d3a6804
David Heidrich <me@bowlingx.com>
2019-12-19 12:17:43 +0100

    fix(exports): added default namespace name export

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 export { Geschichte as default } from './lib/provider'
 export { serializers } from './lib/serializers'
-export { factoryParameters, useBatchQuery, DEFAULT_NAMESPACE } from './lib/store'
+export {
+  factoryParameters,
+  useBatchQuery,
+  DEFAULT_NAMESPACE
+} from './lib/store'
 export { pm } from './lib/utils'
