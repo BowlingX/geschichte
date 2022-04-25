ff5f001945a2fcfdbd32f8e36db039cf3aabca7c
David Heidrich <me@bowlingx.com>
2022-04-25 12:29:25 +0200

    feat: make skip customizable per parameter. You may now pass a 3. parameter to `pm` to define your own skip logic.

---
+++ src/__tests__/skip.ts
@@ -0,0 +1,45 @@
+/* tslint:disable:no-expression-statement no-object-mutation */
+
+import { factoryParameters } from '../lib/store'
+import { defaultSkipValue, pm } from '../lib/utils'
+import { Serializer } from '../lib/serializers'
+
+export const nullableBooleanSerializer: Serializer = {
+  deserialize: (value: string | null) => {
+    if (value === '1') {
+      return true
+    }
+    return null
+  },
+  serialize: (value?: boolean) => (value ? '1' : '0'),
+}
+
+describe('custom skip', () => {
+  const initialValues = { nullable: null }
+  const { parseQueryString, createQueryString } = factoryParameters(
+    {
+      nullable: pm(
+        'nullable',
+        nullableBooleanSerializer,
+        (value, initialValue) =>
+          value === null ? false : defaultSkipValue(value, initialValue)
+      ),
+    },
+    initialValues
+  )
+
+  it('should parse a query string', () => {
+    const result = parseQueryString('?nullable=1')
+    expect(result).toEqual({ nullable: true })
+  })
+
+  it('should parse null', () => {
+    const result = parseQueryString('?nullable=0')
+    expect(result).toEqual({ nullable: null })
+  })
+
+  it('should create a query string', () => {
+    const queryString = createQueryString({ nullable: null })
+    expect(queryString).toEqual('nullable=0')
+  })
+})
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-expression-statement readonly-array no-shadowed-variable */
 import { Draft, enablePatches, produce } from 'immer'
 import memoizeOne from 'memoize-one'
-import { stringify, parse } from 'query-string'
+import { parse, stringify } from 'query-string'
 import {
   createContext,
   useCallback,
@@ -20,7 +20,7 @@ import create, {
   UseBoundStore,
 } from 'zustand'
 // tslint:disable-next-line:no-submodule-imports
-import { subscribeWithSelector, devtools } from 'zustand/middleware'
+import { devtools, subscribeWithSelector } from 'zustand/middleware'
 
 // tslint:disable-next-line:no-submodule-imports
 import shallow from 'zustand/shallow'
@@ -44,9 +44,11 @@ export const StoreContext = createContext<UseBoundStore<
   StoreState<any>
 > | null>(null)
 
-export interface Parameter {
+export interface Parameter<V = any> {
   readonly name: string
-  readonly serializer: Serializer
+  readonly serializer: Serializer<V>
+  // tslint:disable-next-line:no-mixed-interface
+  readonly skipValue: (value?: V, initialValue?: V) => boolean
 }
 
 export interface MappedParameter extends Parameter {
