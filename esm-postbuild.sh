360b9b8fd2777d62bdba99063b29dea6cb9c2b35
David Heidrich <me@bowlingx.com>
2025-03-21 14:00:55 +0100

    fix: removed node10 support for ts

--- esm-postbuild.sh
+++ esm-postbuild.sh
@@ -3,8 +3,3 @@ cat >build/cjs/package.json <<!EOF
     "type": "commonjs"
 }
 !EOF
-
-cp ./build/mjs/lib/adapters/nextjs/index.d.ts nextjs.d.ts
-cp ./build/mjs/lib/adapters/historyjs/index.d.ts historyjs.d.ts
-cp ./build/mjs/lib/adapters/static/index.d.ts static.d.ts
-cp ./build/mjs/lib/adapters/nextjs-app-router/index.d.ts nextjs-app-router.d.ts
